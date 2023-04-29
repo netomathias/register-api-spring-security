3afd2f0063c1ec3435f32500a9f9ab5a4ef2216e
ivonir_mathias_neto <ivonir.mathiasneto@gmail.com>
2023-04-29 10:54:17 -0300

    :wrench: chore: use the placeholder from enviroment variable to jwt secret

--- src/main/java/br/com/registerpoc/registerapipoc/services/impl/JwtService.java
+++ src/main/java/br/com/registerpoc/registerapipoc/services/impl/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,8 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "put your secret here";
+    @Value("${jwt-secret}")
+    private String SECRET_KEY;
 
     public String extractTheUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
